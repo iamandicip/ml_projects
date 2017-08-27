047fc216792eccef21b92ce171f1543bdcf62e04
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2017-08-27 23:38:12 +0200

    added models for classification

--- capstone_project/stock_utils.py
+++ capstone_project/stock_utils.py
@@ -2,8 +2,10 @@ import os
 import errno
 import pandas as pd
 import datetime
+from time import time
 import matplotlib.pyplot as plt
 import pandas_datareader.data as web
+from sklearn.metrics import f1_score
 
 def make_sure_path_exists(path):
     try:
@@ -144,6 +146,42 @@ def preprocess_data(symbol, window, start_date, end_date):
 
     # keep only the rows that have values for the features calculated on the window
     return df[window:]
+
+def train_classifier(clf, X_train, y_train):
+    ''' Fits a classifier to the training data. '''
+
+    # Start the clock, train the classifier, then stop the clock
+    start = time()
+    clf.fit(X_train, y_train)
+    end = time()
+
+    # Print the results
+    print ("Trained model in {:.4f} seconds".format(end - start))
+
+def predict_labels(clf, features, target):
+    ''' Makes predictions using a fit classifier based on F1 score. '''
+
+    # Start the clock, make predictions, then stop the clock
+    start = time()
+    y_pred = clf.predict(features)
+    end = time()
+
+    # Print and return results
+    print ("Made predictions in {:.4f} seconds.".format(end - start))
+    return f1_score(target.values, y_pred, pos_label=1)
+
+def train_predict(clf, X_train, y_train, X_test, y_test):
+    ''' Train and predict using a classifer based on F1 score. '''
+
+    # Indicate the classifier and the training set size
+    print ("Training a {0} using a training set size of {1}...".format(clf.__class__.__name__, len(X_train)))
+
+    # Train the classifier
+    train_classifier(clf, X_train, y_train)
+
+    # Print the results of prediction for both training and testing
+    print ("F1 score for training set: {:.4f}.".format(predict_labels(clf, X_train, y_train)))
+    print ("F1 score for test set: {:.4f}.".format(predict_labels(clf, X_test, y_test)))
 
 if __name__ == '__main__':
     start = datetime.datetime(2016,1,1)
